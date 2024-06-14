e3975cb3d5dc1dc2d60d9066ec1635becef6c3ba
jeeswan <bajrajeeswan@gmail.com>
2024-06-14 11:56:52 +0545

    fifth commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// let number = 1;
+// let num2 = 1.2;
+
+// console.log(number);
+// console.log(num2);
+
+// num2 = 'apple';
+// console.log(num2);
+
+
+
+// let gods = ['ram', 'shyam', 'krishna'];
+// let wepons = ['bow', 'arrow', 'sudarsan'];
+
+// for (var item of gods){
+//     console.log(item);
+//     for(var item of wepons){
+//         //
+//     }
+//     console.log(item);
+// }
+
+
+
+// const myname = 'Aaalu';
+// myname = 'Padalu';
+
+const myarr = [1,2,3];
+console.log(myarr);
+myarr[1] = 5;
+console.log(myarr);
+myarr.push(7);
+console.log(myarr);
